37984484983cbfed03b330a70ee5d480460b90f8
Aabhiraj412 <aabhiraj412@gmail.com>
2024-12-02 18:59:19 +0530

    Multi Device

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,37 @@
-const io = require('socket.io')(8000)
+// Set up Socket.IO with proper CORS settings
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: "http://10.0.34.46:5500", // Frontend origin
+        methods: ["GET", "POST", "PUT"], // Allowed methods
+        allowedHeaders: ["Content-Type"], // Headers to allow
+        credentials: true // Allow cookies if needed
+    }
+});
 
-const users = {}
-// Access-Control-Allow-Origin: "http://localhost:5500"
-// Access-Control-Allow-Methods: GET, POST, PUT
-// Access-Control-Allow-Headers: Content-Type
+// Object to store connected users
+const users = {};
 
-io.on('connection',socket=>{
-    socket.on('new-user-join',name=>{
-        console.log(name +" Joined")
-        users[socket.id]=name
-        socket.broadcast.emit('user-join',name)
-    })
+// Listen for Socket.IO connections
+io.on('connection', (socket) => {
+    // When a new user joins
+    socket.on('new-user-join', (name) => {
+        console.log(`${name} joined`);
+        users[socket.id] = name; // Map the user's name to their socket ID
+        socket.broadcast.emit('user-join', name); // Notify others of the new user
+    });
 
-    socket.on('send',message=>{
-        socket.broadcast.emit('recived',{message: message,name: users[socket.id]})
-    })
+    // When a user sends a message
+    socket.on('send', (message) => {
+        socket.broadcast.emit('recived', {
+            message: message,
+            name: users[socket.id]
+        });
+    });
 
-    socket.on('disconnect',name=>{
-        console.log(name +" left")
-        socket.broadcast.emit('leave', users[socket.id])
-        delete users[socket.id]
-    })
-})
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        console.log(`${users[socket.id]} left`);
+        socket.broadcast.emit('leave', users[socket.id]); // Notify others of the user's departure
+        delete users[socket.id]; // Remove user from the list
+    });
+});
--- Frontend/client.js
+++ Frontend/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000')
+const socket = io('http://10.0.34.46:8000')
 
 const id = document.getElementById('id')
 const form = document.getElementById('sent')
